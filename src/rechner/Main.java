339588ef755e239656d49d9e930533f14ce27342
Chris <Cloud>
2014-10-29 16:59:16 +0100

    Changed main

--- src/rechner/Main.java
+++ src/rechner/Main.java
@@ -8,7 +8,10 @@ package rechner;
 public class Main {
     
     
-    
+    /**
+     * OVertaken by Chris
+     * @param args 
+     */
     public static void main(String[] args) {
         
         new Rechner();
